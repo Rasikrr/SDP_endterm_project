324e6bf2987483c86781dc93a4a07f9215bba84c
Amir Kurmanbekov <amir.kurmanbekov@gmail.com>
2024-11-05 15:25:53 +0500

    using of factory method

--- users/views.py
+++ users/views.py
@@ -1,19 +1,24 @@
-from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect
-from django.urls import reverse_lazy, reverse
-from django.contrib.auth.models import User
-from django.contrib import auth
-from django.contrib import messages
+from django.contrib import auth, messages
 from django.contrib.auth.decorators import login_required
-
-from users.services import signin_service, generate_context, signup_service, profile_edit_service, add_anonymous_user_cart
+from django.contrib.auth.models import User
+from django.shortcuts import HttpResponse, HttpResponseRedirect, redirect, render
+from django.urls import reverse, reverse_lazy
+from main.decorators import log_request
+from orders.models import Order, OrderItem
+from users.forms import CreateUserForm, FormFactory, LoginForm, ProfileForm
 from users.models import CustomUser
-from users.forms import LoginForm, CreateUserForm, ProfileForm
+from users.services import (
+    add_anonymous_user_cart,
+    generate_context,
+    profile_edit_service,
+    signin_service,
+    signup_service,
+)
 
 
-@log_request
 def signin(request):
     if request.method == "POST":
-        form = LoginForm(request.POST)
+        form = FormFactory.create_form("login", request.POST)
         user = signin_service(form=form)
         if user:
             auth.login(request, user)
@@ -29,14 +34,14 @@ def signin(request):
             messages.warning(request, "Неверная почта или пароль")
             return redirect("user:signin")
     else:
-        form = LoginForm()
+        form = FormFactory.create_form("login")
     context = generate_context(form=form)
     return render(request, "signin.html", context=context)
 
 
 def registration(request):
     if request.method == "POST":
-        form = CreateUserForm(request.POST)
+        form = FormFactory.create_form("signup", request.POST)
         user = signup_service(form)
         if user:
 
@@ -46,7 +51,7 @@ def registration(request):
             messages.success(request, "Вы успешно зарегестрированы и вошли в аккаунт")
             return redirect("main:index")
     else:
-        form = CreateUserForm()
+        form = FormFactory.create_form("signup")
     context = generate_context(form=form)
     return render(request, "registration.html", context=context)
 
@@ -54,19 +59,37 @@ def registration(request):
 @login_required
 def profile(request):
     if request.method == "POST":
-        form = ProfileForm(request.POST, instance=request.user, files=request.FILES)
+        form = FormFactory.create_form(
+            "profile", request.POST, instance=request.user, files=request.FILES
+        )
         response = profile_edit_service(form)
         if response:
-            print("YESS")
             messages.success(request, "Данные успешно обновлены")
             return redirect("user:profile")
     else:
-        form = ProfileForm()
+        form = FormFactory.create_form("profile", instance=request.user)
+    user_orders = Order.objects.filter(user=request.user)
+    orders_items = OrderItem.objects.filter(order__in=user_orders)
+    order_with_items = {}
+    for order in user_orders:
+        order_with_items[order] = []
+
+    overall = 0.0
+    for item in orders_items:
+        overall += float(item.quantity) * float(item.price)
+        order_with_items[item.order].append(item)
+    orders = []
+    for order in order_with_items:
+        orders.append([order, order_with_items[order]])
     context = generate_context(form=form)
+    context["orders"] = orders
+    context["overall"] = round(overall, 2)
     return render(request, "profile.html", context=context)
+
 
 def users_cart(request):
     return render(request, "users/users_cart.html")
+
 
 def logout(request):
     auth.logout(request)
